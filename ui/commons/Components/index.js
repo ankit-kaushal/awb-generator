5a3faa1b9b7835e38ee9fd96a2352d0f91cc26a7
Ankit Kaushal <ankit.kaushal@cogoport.com>
2024-01-21 01:26:07 +0530

    removed import

--- ui/commons/Components/index.js
+++ ui/commons/Components/index.js
@@ -1,7 +1,3 @@
-export { default as Input } from './Input';
 export { default as Textarea } from './Textarea';
-export { default as Select } from './Select';
-export { default as Datepicker } from './Datepicker';
-export { default as Button } from './Button';
 export { default as Loader } from './Loader';
 export { default as Modal } from './Modal';
